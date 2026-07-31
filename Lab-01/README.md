b90bf72cc2132ae39990ed2255e6e2383f262e79
gaurityagi13 <gauriityagii13@gmail.com>
2026-07-31 18:11:49 +0530

    Lab 01 Submission

---
+++ Lab-01/README.md
@@ -0,0 +1,19 @@
+# Node.js Lab Assignment 01
+
+## Student Details
+
+Name: Gauri Tyagi
+Scholar Number: 23145006
+Course: BCA
+Semester: VII
+Subject: CS403NOD - Node.js
+Lab Number: 01
+Date: 31 July 2026
+
+## Practical Description
+
+This practical introduces Node.js installation, project initialization using npm, creating a simple Node.js program, understanding variables and data types, and learning the difference between Browser JavaScript and Node.js.
+
+## Problems Faced
+
+No major issues encountered.
---
+++ Lab-01/app.js
@@ -0,0 +1,40 @@
+console.log("Welcome to Node.js");
+
+console.log("Name: Gauri Tyagi");
+console.log("Scholar Number: 23145006");
+console.log("Course: BCA");
+console.log("Semester: VII");
+console.log("College: DSVV");
+
+// Task 5
+console.log("\nHello Node.js");
+console.log("Learning Backend Development");
+console.log("Today's Lab Completed Successfully");
+
+// Task 6
+let name = "Gauri Tyagi";
+let scholarNumber = "23145006";
+let semester = "VII";
+let course = "BCA";
+let college = "DSVV";
+
+console.log("\nVariables");
+console.log(name);
+console.log(scholarNumber);
+console.log(semester);
+console.log(course);
+console.log(college);
+
+// Task 7
+let language = "JavaScript";
+let age = 20;
+let isStudent = true;
+let city;
+let marks = null;
+
+console.log("\nData Types");
+console.log(typeof language);
+console.log(typeof age);
+console.log(typeof isStudent);
+console.log(typeof city);
+console.log(typeof marks);
